db4a8687f5a13824172fe3deaf3978c1f8acbef6
charlie  lee <ctlee9096@gmail.com>
2018-12-23 22:00:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const returnLastTwoDrivers= function(drivers) {
 }
 
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier
+  }
+}
